43bf254d5b26cbfd601f98d9bf6869459028e86a
Neville Li <neville@spotify.com>
2019-10-22 10:46:56 -0400

    update cats

--- cats/src/main/scala/magnolia/cats/EqDerivation.scala
+++ cats/src/main/scala/magnolia/cats/EqDerivation.scala
@@ -8,13 +8,13 @@ import scala.language.experimental.macros
 object EqDerivation {
   type Typeclass[T] = Eq[T]
 
-  def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] = Eq.instance[T] { (x, y) =>
+  def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] = Eq.instance { (x, y) =>
     caseClass.parameters.forall { p =>
       p.typeclass.eqv(p.dereference(x), p.dereference(y))
     }
   }
 
-  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Eq[T] = Eq.instance[T] { (x, y) =>
+  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Eq[T] = Eq.instance { (x, y) =>
     sealedTrait.dispatch(x) { sub =>
       sub.cast.isDefinedAt(y) && sub.typeclass.eqv(sub.cast(x), sub.cast(y))
     }
--- cats/src/main/scala/magnolia/cats/SemigroupDerivation.scala
+++ cats/src/main/scala/magnolia/cats/SemigroupDerivation.scala
@@ -8,19 +8,21 @@ import scala.language.experimental.macros
 object SemigroupDerivation {
   type Typeclass[T] = Semigroup[T]
 
-  def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] = new Semigroup[T] {
-    override def combine(x: T, y: T): T =
-      caseClass.construct { p =>
-        p.typeclass.combine(p.dereference(x), p.dereference(y))
+  def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] = Semigroup.instance { (x, y) =>
+    caseClass.construct { p =>
+      p.typeclass.combine(p.dereference(x), p.dereference(y))
+    }
+  }
+
+  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Typeclass[T] =
+    Semigroup.instance { (x, y) =>
+      val sub = sealedTrait.subtypes.find(_.cast.isDefinedAt(x)).get
+      if (sub.cast.isDefinedAt(y)) {
+        sub.typeclass.combine(sub.cast(x), sub.cast(y))
+      } else {
+        throw new IllegalArgumentException(s"Incompatible subtypes $x and $y")
       }
-
-    //override def combineAllOption(as: IterableOnce[T]): Option[T] = ???
-  }
-
-  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Typeclass[T] = new Semigroup[T] {
-    override def combine(x: T, y: T): T = ???
-    //override def combineAllOption(as: IterableOnce[T]): Option[T] = ???
-  }
+    }
 
   implicit def gen[T]: Semigroup[T] = macro Magnolia.gen[T]
 }
