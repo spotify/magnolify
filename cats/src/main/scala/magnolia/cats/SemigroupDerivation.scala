8d05e0a9104fb7371aafcb68b72e56ab31340127
Neville Li <neville@spotify.com>
2019-10-22 10:56:03 -0400

    remove unlawful Semigroup dispatch

--- cats/src/main/scala/magnolia/cats/SemigroupDerivation.scala
+++ cats/src/main/scala/magnolia/cats/SemigroupDerivation.scala
@@ -14,15 +14,7 @@ object SemigroupDerivation {
     }
   }
 
-  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Typeclass[T] =
-    Semigroup.instance { (x, y) =>
-      val sub = sealedTrait.subtypes.find(_.cast.isDefinedAt(x)).get
-      if (sub.cast.isDefinedAt(y)) {
-        sub.typeclass.combine(sub.cast(x), sub.cast(y))
-      } else {
-        throw new IllegalArgumentException(s"Incompatible subtypes $x and $y")
-      }
-    }
+  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Typeclass[T] = ???
 
   implicit def gen[T]: Semigroup[T] = macro Magnolia.gen[T]
 }
