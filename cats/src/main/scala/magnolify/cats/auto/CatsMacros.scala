e3e71948ef129f4a9c4698362f7ad8a5422ca2a1
Neville Li <neville.lyh@gmail.com>
2021-02-23 18:19:30 -0500

    make cats derivation low priority

--- cats/src/main/scala/magnolify/cats/auto/CatsMacros.scala
+++ cats/src/main/scala/magnolify/cats/auto/CatsMacros.scala
@@ -24,128 +24,119 @@ import scala.language.experimental.macros
 import scala.reflect.macros._
 
 private object CatsMacros {
-  def genEqMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genEqMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.EqDerivation.apply[$wtt]"""
   }
 
-  def genHashMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genHashMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.HashDerivation.apply[$wtt]"""
   }
 
-  def genSemigroupMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genSemigroupMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.SemigroupDerivation.apply[$wtt]"""
   }
 
-  def genCommutativeSemigroupMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genCommutativeSemigroupMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.CommutativeSemigroupDerivation.apply[$wtt]"""
   }
 
-  def genBandMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genBandMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.BandDerivation.apply[$wtt]"""
   }
 
-  def genMonoidMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genMonoidMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.MonoidDerivation.apply[$wtt]"""
   }
 
-  def genCommutativeMonoidMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genCommutativeMonoidMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.CommutativeMonoidDerivation.apply[$wtt]"""
   }
 
-  def genGroupMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genGroupMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.GroupDerivation.apply[$wtt]"""
   }
 
-  def genCommutativeGroupMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genCommutativeGroupMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.CommutativeGroupDerivation.apply[$wtt]"""
   }
 
-  def genShowMacro[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
+  def genShowMacro[T: c.WeakTypeTag](c: whitebox.Context)(lp: c.Tree): c.Tree = {
     import c.universe._
     val wtt = weakTypeTag[T]
     q"""_root_.magnolify.cats.semiauto.ShowDerivation.apply[$wtt]"""
   }
 }
 
-trait LowPriorityImplicits extends LowPriorityImplicits1 {
-  // workaround for ambiguous implicit values with cats
-  implicit def catsKernelStdHashForList[A: Hash]: Hash[List[A]] =
-    new ListHash[A]
-  implicit def catsKernelStdHashForOption[A: Hash]: Hash[Option[A]] =
-    new OptionHash[A]
-}
-
-trait LowPriorityImplicits1
+trait LowPriorityImplicits
     extends LowPriorityGenCommmutativeGroup
     with LowPriorityGenHash
-    with LowPriorityGenShow {
-  implicit def catsKernelStdOrderForList[A: Order]: Order[List[A]] =
-    new ListOrder[A]
-  implicit def catsKernelStdPartialOrderForList[A: PartialOrder]: PartialOrder[List[A]] =
-    new ListPartialOrder[A]
-  implicit def catsKernelStdOrderForOption[A: Order]: Order[Option[A]] =
-    new OptionOrder[A]
-  implicit def catsKernelStdPartialOrderForOption[A: PartialOrder]: PartialOrder[Option[A]] =
-    new OptionPartialOrder[A]
-}
+    with LowPriorityGenShow
 
 trait LowPriorityGenCommmutativeGroup extends LowPriorityGenGroup {
-  implicit def genCommutativeGroup[T]: CommutativeGroup[T] =
+  implicit def genCommutativeGroup[T](implicit lp: shapeless.LowPriority): CommutativeGroup[T] =
     macro CatsMacros.genCommutativeGroupMacro[T]
 }
 
 trait LowPriorityGenGroup extends LowPriorityGenCommmutativeMonoid {
-  implicit def genGroup[T]: Group[T] = macro CatsMacros.genGroupMacro[T]
+  implicit def genGroup[T](implicit lp: shapeless.LowPriority): Group[T] =
+    macro CatsMacros.genGroupMacro[T]
 }
 
 trait LowPriorityGenCommmutativeMonoid extends LowPriorityGenMonoid {
-  implicit def genCommutativeMonoid[T]: CommutativeMonoid[T] =
+  implicit def genCommutativeMonoid[T](implicit lp: shapeless.LowPriority): CommutativeMonoid[T] =
     macro CatsMacros.genCommutativeMonoidMacro[T]
 }
 
 trait LowPriorityGenMonoid extends LowPriorityGenBand {
-  implicit def genMonoid[T]: Monoid[T] = macro CatsMacros.genMonoidMacro[T]
+  implicit def genMonoid[T](implicit lp: shapeless.LowPriority): Monoid[T] =
+    macro CatsMacros.genMonoidMacro[T]
 }
 
 trait LowPriorityGenBand extends LowPriorityGenCommutativeSemigroup {
-  implicit def genBand[T]: Band[T] = macro CatsMacros.genBandMacro[T]
+  implicit def genBand[T](implicit lp: shapeless.LowPriority): Band[T] =
+    macro CatsMacros.genBandMacro[T]
 }
 
 trait LowPriorityGenCommutativeSemigroup extends LowPriorityGenSemigroup {
-  implicit def genCommutativeSemigroup[T]: CommutativeSemigroup[T] =
+  implicit def genCommutativeSemigroup[T](implicit
+    lp: shapeless.LowPriority
+  ): CommutativeSemigroup[T] =
     macro CatsMacros.genCommutativeSemigroupMacro[T]
 }
 
 trait LowPriorityGenSemigroup {
-  implicit def genSemigroup[T]: Semigroup[T] = macro CatsMacros.genSemigroupMacro[T]
+  implicit def genSemigroup[T](implicit lp: shapeless.LowPriority): Semigroup[T] =
+    macro CatsMacros.genSemigroupMacro[T]
 }
 
 trait LowPriorityGenHash extends LowPriorityGenEq {
-  implicit def genHash[T]: Hash[T] = macro CatsMacros.genHashMacro[T]
+  implicit def genHash[T](implicit lp: shapeless.LowPriority): Hash[T] =
+    macro CatsMacros.genHashMacro[T]
 }
 
 trait LowPriorityGenEq {
-  implicit def genEq[T]: Eq[T] = macro CatsMacros.genEqMacro[T]
+  implicit def genEq[T](implicit lp: shapeless.LowPriority): Eq[T] = macro CatsMacros.genEqMacro[T]
 }
 
 trait LowPriorityGenShow {
-  implicit def genShow[T]: Show[T] = macro CatsMacros.genShowMacro[T]
+  implicit def genShow[T](implicit lp: shapeless.LowPriority): Show[T] =
+    macro CatsMacros.genShowMacro[T]
 }
