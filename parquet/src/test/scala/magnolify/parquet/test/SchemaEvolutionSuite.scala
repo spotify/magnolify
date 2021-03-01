229077ae25cf5632ceda26d084b3048f954d9a50
Neville Li <neville.lyh@gmail.com>
2021-03-01 13:54:03 -0500

    Avro Enum evolution failure case

--- parquet/src/test/scala/magnolify/parquet/test/SchemaEvolutionSuite.scala
+++ parquet/src/test/scala/magnolify/parquet/test/SchemaEvolutionSuite.scala
@@ -19,15 +19,10 @@ package magnolify.parquet.test
 import magnolify.parquet._
 import magnolify.shims.JavaConverters._
 import magnolify.test._
-import org.apache.avro.generic.{GenericRecord, GenericRecordBuilder}
+import org.apache.avro.generic.{GenericData, GenericEnumSymbol, GenericRecord, GenericRecordBuilder}
 import org.apache.avro.{JsonProperties, Schema}
 import org.apache.hadoop.conf.Configuration
-import org.apache.parquet.avro.{
-  AvroParquetReader,
-  AvroParquetWriter,
-  AvroReadSupport,
-  GenericDataSupplier
-}
+import org.apache.parquet.avro.{AvroParquetReader, AvroParquetWriter, AvroReadSupport, GenericDataSupplier}
 
 object SchemaEvolutionSuite {
   private val namespace = "magnolify.parquet"
@@ -50,6 +45,13 @@ object SchemaEvolutionSuite {
     (v1, v2)
   }
 
+  private val types1 = List("Checking", "Saving")
+  private val types2 = types1 :+ "OffShore"
+  val accountTypeSchema1: Schema =
+    Schema.createEnum("AccountType", "", namespace, types1.asJava)
+  val accountTypeSchema2: Schema =
+    Schema.createEnum("AccountType", "", namespace, types2.asJava)
+
   /*
   V2 adds 3 fields
   - Nested nullable string field "location.zip"
@@ -61,10 +63,12 @@ object SchemaEvolutionSuite {
     def name = new Schema.Field("name", Schema.create(Schema.Type.STRING), "", null)
 
     val v1 = Schema.createRecord("UserV1", "", namespace, false)
+    val type1 = new Schema.Field("account_type", accountTypeSchema1, "", null)
     val location1 = new Schema.Field("location", locationSchema1, "", null)
-    v1.setFields(List(id, name, location1).asJava)
+    v1.setFields(List(id, name, type1, location1).asJava)
 
     val v2 = Schema.createRecord("UserV2", "", namespace, false)
+    val type2 = new Schema.Field("account_type", accountTypeSchema2, "", null)
     val location2 = new Schema.Field("location", locationSchema2, "", null)
     val email = new Schema.Field("email", nullableString, "", nullVal)
     val aliases = new Schema.Field(
@@ -73,7 +77,7 @@ object SchemaEvolutionSuite {
       "",
       java.util.Collections.emptyList()
     )
-    v2.setFields(List(id, name, location2, email, aliases).asJava)
+    v2.setFields(List(id, name, type2, location2, email, aliases).asJava)
 
     (v1, v2)
   }
@@ -88,16 +92,18 @@ object SchemaEvolutionSuite {
       .set("zip", zip)
       .build()
 
-  def avroUser1(id: Long, name: String, country: String, state: String): GenericRecord =
+  def avroUser1(id: Long, name: String, tpe: String, country: String, state: String): GenericRecord =
     new GenericRecordBuilder(userSchema1)
       .set("id", id)
       .set("name", name)
+      .set("account_type", new GenericData.EnumSymbol(accountTypeSchema1, tpe))
       .set("location", avroLocation1(country, state))
       .build()
 
   def avroUser2(
     id: Long,
     name: String,
+    tpe: String,
     country: String,
     state: String,
     zip: String,
@@ -107,6 +113,7 @@ object SchemaEvolutionSuite {
     new GenericRecordBuilder(userSchema2)
       .set("id", id)
       .set("name", name)
+      .set("account_type", new GenericData.EnumSymbol(accountTypeSchema2, tpe))
       .set("location", avroLocation2(country, state, zip))
       .set("email", email)
       .set("aliases", aliases.asJava)
@@ -124,24 +131,27 @@ object SchemaEvolutionSuite {
   )
 
   val avro1: Seq[GenericRecord] = Seq(
-    avroUser1(0, "Alice", "US", "NY"),
-    avroUser1(1, "Bob", "US", "NJ"),
-    avroUser1(2, "Carol", "US", "CT"),
-    avroUser1(3, "Dan", "US", "MA")
-  )
+    avroUser1(0, "Alice", "Checking", "US", "NY"),
+    avroUser1(1, "Bob", "Saving", "US", "NJ"),
+    avroUser1(2, "Carol", "Checking", "US", "CT"),
+    avroUser1(3, "Dan", "Saving", "US", "MA"))
+
+  val avro2as1: Seq[GenericRecord] =
+    avro1 :+ avroUser1(4, "Ed", "OffShore", "US", "CA")
 
   val avro2: Seq[GenericRecord] = Seq(
-    avroUser2(0, "Alice", "US", "NY", "12345", "alice@aol.com", Seq("Ada", "Ana")),
-    avroUser2(1, "Bob", "US", "NJ", null, null, Nil),
-    avroUser2(2, "Carol", "US", "CT", null, "carol@aol.com", Nil),
-    avroUser2(3, "Dan", "US", "MA", "54321", null, Nil)
+    avroUser2(0, "Alice", "Checking", "US", "NY", "12345", "alice@aol.com", Seq("Ada", "Ana")),
+    avroUser2(1, "Bob", "Saving", "US", "NJ", null, null, Nil),
+    avroUser2(2, "Carol", "Checking", "US", "CT", null, "carol@aol.com", Nil),
+    avroUser2(3, "Dan", "Saving", "US", "MA", "54321", null, Nil),
+    avroUser2(4, "Ed", "OffShore", "US", "CA", "98765", "ed@aol.com", Seq("Edward"))
   )
 
   val avro1as2: Seq[GenericRecord] = Seq(
-    avroUser2(0, "Alice", "US", "NY", null, null, null),
-    avroUser2(1, "Bob", "US", "NJ", null, null, null),
-    avroUser2(2, "Carol", "US", "CT", null, null, null),
-    avroUser2(3, "Dan", "US", "MA", null, null, null)
+    avroUser2(0, "Alice", "Checking", "US", "NY", null, null, null),
+    avroUser2(1, "Bob", "Saving", "US", "NJ", null, null, null),
+    avroUser2(2, "Carol", "Checking", "US", "CT", null, null, null),
+    avroUser2(3, "Dan", "Saving", "US", "MA", null, null, null)
   )
 
   val scala1: Seq[User1] = Seq(
@@ -252,91 +262,114 @@ class SchemaEvolutionSuite extends MagnolifySuite {
     assertEquals(readAvro(avroBytes1, userSchema2), avro1as2)
   }
 
+  println("=" * 100)
+  readAvro(avroBytes2, userSchema1).foreach(println)
+  println("=" * 100)
+  avro2as1.foreach(println)
+
   test("Avro V2 => Avro V1") {
-    assertEquals(readAvro(avroBytes2, userSchema1), avro1)
+    val ed1 = readAvro(avroBytes2, userSchema1).last
+    val ed2 = avro2as1.last
+    println("ED1 " + ed1)
+    println("ED2 " + ed2)
+
+    val tpe1 = ed1.get("account_type")
+    val tpe2 = ed2.get("account_type")
+    println("ED1 tpe " + tpe1)
+    println("ED2 tpe " + tpe2)
+
+    val s1 = tpe1.asInstanceOf[GenericEnumSymbol[_]].getSchema
+    val s2 = tpe2.asInstanceOf[GenericEnumSymbol[_]].getSchema
+    println("ED1 schema " + s1)
+    println("ED2 schema " + s2)
+
+    /// NPEs in EnumSchema.getEnumOrdinal
+    println("ED1 ord " + s1.getEnumOrdinal(tpe1.toString))
+    println("ED2 ord " + s2.getEnumOrdinal(tpe2.toString))
+//    assertEquals(readAvro(avroBytes2, userSchema1), avro2as1)
   }
 
   //////////////////////////////////////////////////
 
-  test("Scala V1 => Scala V1") {
-    assertEquals(readScala[User1](scalaBytes1), scala1)
-  }
-
-  test("Scala V2 => Scala V2") {
-    assertEquals(readScala[User2](scalaBytes2), scala2)
-  }
-
-  test("Scala V1 => Scala V2") {
-    assertEquals(readScala[User2](scalaBytes1), scala1as2)
-  }
-
-  test("Scala V2 => Scala V1") {
-    assertEquals(readScala[User1](scalaBytes2), scala1)
-  }
-
-  //////////////////////////////////////////////////
-
-  test("Scala Compat V1 => Scala Compat V1") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readScala[User1](scalaCompatBytes1), scala1)
-  }
-
-  test("Scala Compat V2 => Scala Compat V2") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readScala[User2](scalaCompatBytes2), scala2)
-  }
-
-  test("Scala Compat V1 => Scala Compat V2") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readScala[User2](scalaCompatBytes1), scala1as2)
-  }
-
-  test("Scala Compat V2 => Scala Compat V1") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readScala[User1](scalaCompatBytes2), scala1)
-  }
-
-  //////////////////////////////////////////////////
-
-  test("Avro V1 => Scala V1") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readScala[User1](avroBytes1), scala1)
-  }
-
-  test("Avro V2 => Scala V2") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readScala[User2](avroBytes2), scala2)
-  }
-
-  test("Avro V1 => Scala V2") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readScala[User2](avroBytes1), scala1as2)
-  }
-
-  test("Avro V2 => Scala V1") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readScala[User1](avroBytes2), scala1)
-  }
-
-  //////////////////////////////////////////////////
-
-  test("Scala V1 => Avro V1") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readAvro(scalaCompatBytes1, userSchema1), avro1)
-  }
-
-  test("Scala V2 => Avro V2") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readAvro(scalaCompatBytes2, userSchema2), avro2)
-  }
-
-  test("Scala V1 => Avro V2") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readAvro(scalaCompatBytes1, userSchema2), avro1as2)
-  }
-
-  test("Scala V2 => Avro V1") {
-    import magnolify.parquet.ParquetArray.AvroCompat._
-    assertEquals(readAvro(scalaCompatBytes2, userSchema1), avro1)
-  }
+//  test("Scala V1 => Scala V1") {
+//    assertEquals(readScala[User1](scalaBytes1), scala1)
+//  }
+//
+//  test("Scala V2 => Scala V2") {
+//    assertEquals(readScala[User2](scalaBytes2), scala2)
+//  }
+//
+//  test("Scala V1 => Scala V2") {
+//    assertEquals(readScala[User2](scalaBytes1), scala1as2)
+//  }
+//
+//  test("Scala V2 => Scala V1") {
+//    assertEquals(readScala[User1](scalaBytes2), scala1)
+//  }
+//
+//  //////////////////////////////////////////////////
+//
+//  test("Scala Compat V1 => Scala Compat V1") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readScala[User1](scalaCompatBytes1), scala1)
+//  }
+//
+//  test("Scala Compat V2 => Scala Compat V2") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readScala[User2](scalaCompatBytes2), scala2)
+//  }
+//
+//  test("Scala Compat V1 => Scala Compat V2") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readScala[User2](scalaCompatBytes1), scala1as2)
+//  }
+//
+//  test("Scala Compat V2 => Scala Compat V1") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readScala[User1](scalaCompatBytes2), scala1)
+//  }
+//
+//  //////////////////////////////////////////////////
+//
+//  test("Avro V1 => Scala V1") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readScala[User1](avroBytes1), scala1)
+//  }
+//
+//  test("Avro V2 => Scala V2") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readScala[User2](avroBytes2), scala2)
+//  }
+//
+//  test("Avro V1 => Scala V2") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readScala[User2](avroBytes1), scala1as2)
+//  }
+//
+//  test("Avro V2 => Scala V1") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readScala[User1](avroBytes2), scala1)
+//  }
+//
+//  //////////////////////////////////////////////////
+//
+//  test("Scala V1 => Avro V1") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readAvro(scalaCompatBytes1, userSchema1), avro1)
+//  }
+//
+//  test("Scala V2 => Avro V2") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readAvro(scalaCompatBytes2, userSchema2), avro2)
+//  }
+//
+//  test("Scala V1 => Avro V2") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readAvro(scalaCompatBytes1, userSchema2), avro1as2)
+//  }
+//
+//  test("Scala V2 => Avro V1") {
+//    import magnolify.parquet.ParquetArray.AvroCompat._
+//    assertEquals(readAvro(scalaCompatBytes2, userSchema1), avro1)
+//  }
 }
