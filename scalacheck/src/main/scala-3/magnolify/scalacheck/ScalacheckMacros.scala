8d3282c8622680fd16d5cdea0d3183f7156ea37a
Michel Davit <micheld@spotify.com>
2024-01-03 12:01:15 +0100

    Use implicit instead of given

--- scalacheck/src/main/scala-3/magnolify/scalacheck/ScalacheckMacros.scala
+++ scalacheck/src/main/scala-3/magnolify/scalacheck/ScalacheckMacros.scala
@@ -3,7 +3,9 @@ package magnolify.scalacheck
 import org.scalacheck.{Arbitrary, Cogen}
 
 import scala.deriving.Mirror
-trait AutoDerivations:
 
-  inline given genArbitrary[T](using Mirror.Of[T]): Arbitrary[T] = ArbitraryDerivation.derivedMirror[T]
-  inline given genCogen[T](using Mirror.Of[T]): Cogen[T] = CogenDerivation.derivedMirror[T]
+trait AutoDerivations:
+  inline implicit def genArbitrary[T](using Mirror.Of[T]): Arbitrary[T] =
+    ArbitraryDerivation.derivedMirror[T]
+  inline implicit def genCogen[T](using Mirror.Of[T]): Cogen[T] =
+    CogenDerivation.derivedMirror[T]
